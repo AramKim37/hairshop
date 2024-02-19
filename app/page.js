7f298214c9007e1a9242c869d0c29a0d6e3a0c16
AramKim37 <aramkim37@gmail.com>
2024-02-18 23:35:42 -0700

    fixed header component

--- app/page.js
+++ app/page.js
@@ -2,5 +2,9 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="w-full min-h-screen flex flex-col items-center justify-center">
+      <Header />
+    </main>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,47 @@ import React from "react";
 import { PiScissorsThin } from "react-icons/pi";
 
 const Header = () => {
-  return <div>hello</div>;
+  return (
+    <div className="w-full fixed top-20 flex flex-col items-center justify-center mt-10 px-10">
+      <div className="w-full">
+        <h1 className="flex flex-col font-extrabold text-2xl">
+          안녕하세요. <span>카카오헤어샵입니다.</span>
+        </h1>
+      </div>
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>{" "}
+      <div className="flex flex-col">
+        <PiScissorsThin />
+        <h1>헤어샵</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

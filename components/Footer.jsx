2918af9d83c5803a65bb472abb86e9843a8cc599
AramKim37 <aramkim37@gmail.com>
2024-02-27 21:32:48 -0700

    building footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h1>이용약관. 개인정보처리방침. 사업자정보확인</h1>
+      <p>대표자명: 원종석, 권규석 | 사업자 등록번호: 120-86=08810</p>
+    </div>
+  );
 };
 
 export default Footer;
